1aaa9cb9c6378f3c1b32de4a76e5ef93dfbe10a8
임준범 <leamjunbeom@naver.com>
2024-02-01 16:46:40 +0900

    화면 기능 구현

---
+++ src/main/java/com/example/blog/dto/ArticleViewResponse.java
@@ -0,0 +1,24 @@
+package com.example.blog.dto;
+
+import java.time.LocalDateTime;
+
+import com.example.blog.domain.Article;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class ArticleViewResponse {
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+
+    public ArticleViewResponse(Article article) {
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.content = article.getContent();
+        this.createdAt = article.getCreatedAt();
+    }
+}
